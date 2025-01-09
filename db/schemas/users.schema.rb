cd78125361d5ff5fb6aaa1118137cc569a2ab72f
ONO-ONO-ONO <yuki.ono0213@gmail.com>
2025-01-09 16:39:52 +0900

    シングルクォーテーションの部分を修正

--- db/schemas/users.schema.rb
+++ db/schemas/users.schema.rb
@@ -1,6 +1,6 @@
-create_table :users, force: :cascade, charset: 'utf8mb4', collation: 'utf8mb4_bin' do |t|
-  t.string  :name, null: false, default: ''
+create_table :users, force: :cascade, charset: "utf8mb4", collation: "utf8mb4_bin" do |t|
+  t.string  :name, null: false, default: ""
   t.integer :age,  null: false, default: 20
 
-  t.index :name, name: 'index_users_on_name'
+  t.index :name, name: "index_users_on_name"
 end
