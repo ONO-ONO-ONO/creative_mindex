5e706b8e6d426decdd213510010e566c7c9f70dc
ONO-ONO-ONO <yuki.ono0213@gmail.com>
2025-02-05 18:32:13 +0900

    修正確認4

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -5,56 +5,118 @@ class MastersController < ApplicationController
       @index_flg = true if params[:q][:big_schema].present?
       @index_flg = true unless params[:q][:name_cont].nil?
       params[:button] = params[:q][:big_schema] if params[:q][:big_schema].present?
+      @schema = check_models(params[:button])
     elsif params[:button].present?
       @index_flg = true
+      @schema = check_models(params[:button])
     end
 
-    # @schema = params[:button] if approval_models.include?(params[:button])
-
-    @schema = check_models(params[:button])
-
-    @q = if @index_flg
-      @schema.safe_constantize.ransack(params[:q])
+    # 想定外の@schemaになっている場合はTOP画面に戻す
+    if @index_flg && approval_models.exclude?(@schema)
+      redirect_to "/"
     else
-      Category.ransack(params[:q])
+      @q = if @index_flg
+        @schema.safe_constantize.ransack(params[:q])
+      else
+        Category.ransack(params[:q])
+      end
+      @masters = @q.result.order(:sort).page(params[:page]).per(10)
     end
-
-    @masters = @q.result.order(:sort).page(params[:page]).per(10)
   end
 
   def edit
-    schema = params[:schema] if params[:schema].present?
-    @master = schema.safe_constantize.find(params["id"])
+    schema = check_models(params[:schema])
+    # 想定外の@schemaになっている場合はTOP画面に戻す
+    if approval_models.exclude?(schema)
+      redirect_to "/"
+    else
+      @master = schema.safe_constantize.find(params["id"])
+    end
   end
 
   def update
     schema = params[:button] if params[:button].present?
-    @master = schema.safe_constantize.find(params["id"])
-    @master.name = params[schema.safe_constantize.lower_case_table_name.to_sym]["name"]
-    @master.sort = params[schema.safe_constantize.lower_case_table_name.to_sym]["sort"]
-    if schema == "RedList"
-      @master.big_code = params[schema.safe_constantize.lower_case_table_name.to_sym]["big_code"]
+    # 想定外の@schemaになっている場合はTOP画面に戻す
+    if approval_models.exclude?(schema)
+      redirect_to "/"
     else
-      @master.eng_name = params[schema.safe_constantize.lower_case_table_name.to_sym]["eng_name"]
-      @master.major_flg = params[schema.safe_constantize.lower_case_table_name.to_sym]["major_flg"]
-    end
-    if @master.save!
-      redirect_to masters_path(button: schema), notice: "更新が成功しました" # 更新成功時
-    else
-      render :edit
+      @master = schema.safe_constantize.find(params["id"])
+      @master.name = params[schema.safe_constantize.lower_case_table_name.to_sym]["name"]
+      @master.sort = params[schema.safe_constantize.lower_case_table_name.to_sym]["sort"]
+      if schema == "RedList"
+        @master.big_code = params[schema.safe_constantize.lower_case_table_name.to_sym]["big_code"]
+      else
+        @master.eng_name = params[schema.safe_constantize.lower_case_table_name.to_sym]["eng_name"]
+        @master.major_flg = params[schema.safe_constantize.lower_case_table_name.to_sym]["major_flg"]
+      end
+      if @master.save!
+        redirect_to masters_path(button: schema), notice: "更新が成功しました" # 更新成功時
+      else
+        render :edit
+      end
     end
   end
 
   def destroy
-    @master = params[:schema].safe_constantize.find(params[:id])
-    @master.destroy
-    redirect_to masters_url, notice: "レコードが削除されました。"
+    schema = params[:schema] if params[:schema].present?
+    # 想定外の@schemaになっている場合はTOP画面に戻す
+    if approval_models.exclude?(schema)
+      redirect_to "/"
+    else
+      @master = schema.safe_constantize.find(params[:id])
+      @master.destroy
+      redirect_to masters_url, notice: "レコードが削除されました。"
+    end
   end
 
   def check_models(model)
     case model
     when "Domain"
-      "Domainaaaa"
+      "Domain"
+    when "Kingdom"
+      "Kingdom"
+    when "SuperPhylum"
+      "SuperPhylum"
+    when "Phylum"
+      "Phylum"
+    when "SubPhylum"
+      "SubPhylum"
+    when "SuperClasses"
+      "SuperClasses"
+    when "Classes"
+      "Classes"
+    when "SubClasses"
+      "SubClasses"
+    when "InfraClasses"
+      "InfraClasses"
+    when "SuperOrder"
+      "SuperOrder"
+    when "Order"
+      "Order"
+    when "SubOrder"
+      "SubOrder"
+    when "InfraOrder"
+      "InfraOrder"
+    when "SuperFamily"
+      "SuperFamily"
+    when "Family"
+      "Family"
+    when "SubFamily"
+      "SubFamily"
+    when "Genuses"
+      "Genuses"
+    when "SubGenuses"
+      "SubGenuses"
+    when "Species"
+      "Species"
+    when "SubSpecies"
+      "SubSpecies"
+    when "RedList"
+      "RedList"
+    when "RedListBigName"
+      "RedListBigName"
+    else
+      "return"
     end
   end
 
@@ -73,6 +135,17 @@ class MastersController < ApplicationController
       InfraClasses
       SuperOrder
       Order
+      SubOrder
+      InfraOrder
+      SuperFamily
+      Family
+      SubFamily
+      Genuses
+      SubGenuses
+      Species
+      SubSpecies
+      RedList
+      RedListBigName
     ].freeze
   end
 end
